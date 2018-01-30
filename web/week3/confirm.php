a4fcab4a0fa04878b6636201db6991444d9fd01e
deedee96 <dang_tuong_hao@yahoo.ca>
2018-01-30 03:54:51 -0700

    fixed minor bug

--- web/week3/confirm.php
+++ web/week3/confirm.php
@@ -2,4 +2,4 @@
 session_start();
 ?>
 
-Your order of $<?php echo htmlspecialchars($_POST["total"])?> has been shipped to $<?php echo htmlspecialchars($_POST["address"]) . ", " . htmlspecialchars($_POST["city"]). ", " . htmlspecialchars($_POST["state"]) . ", " . htmlspecialchars($_POST["zip"])?>
+Your order of $<?php echo htmlspecialchars($_POST["total"])?> has been shipped to <?php echo htmlspecialchars($_POST["address"]) . ", " . htmlspecialchars($_POST["city"]). ", " . htmlspecialchars($_POST["state"]) . ", " . htmlspecialchars($_POST["zip"])?>
