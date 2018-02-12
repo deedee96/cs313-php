42526c0433d6175da1e5ca795868c2d4a69280da
deedee96 <dang_tuong_hao@yahoo.ca>
2018-02-11 22:08:58 -0700

    add and commit

--- web/project/direct.php
+++ web/project/direct.php
@@ -1,17 +1,32 @@
 <?php
-  
-    try
-    {
-      $user = 'postgres';
-      $password = 'password';
-      $db = new PDO('pgsql:host=127.0.0.1;dbname=postgres', $user, $password);
-    }
-    catch (PDOException $ex)
-    {
-      echo 'Error!: ' . $ex->getMessage();
-      die();
+
+    // default Heroku Postgres configuration URL
+    $dbUrl = getenv('DATABASE_URL');
+
+    if (empty($dbUrl)) {
+     // example localhost configuration URL with postgres username and a database called cs313db
+     $dbUrl = "postgres://postgres:password@localhost:5432/postgres";
     }
 
+    $dbopts = parse_url($dbUrl);
+
+
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"],'/');
+
+
+    try {
+     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    }
+    catch (PDOException $ex) {
+     print "<p>error: $ex->getMessage() </p>\n\n";
+     die();
+    }
+
+    
     $username = $_POST['username'];
     $password = $_POST['password'];
 
@@ -30,6 +45,5 @@
         header('Location: index.php');
         exit;
     }
-
 
 ?>
